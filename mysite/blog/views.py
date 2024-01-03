352dfc48ec1eff2947278a15139aa4d71361555e
izorginpasha <izorgin.pasha@yandex.ru>
2024-01-03 20:44:51 +0500

    комментарÐurls

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.http import Http404
 
+
+def post_detail(request, id):
+    try:
+        post = get_object_or_404(Post,
+                                 id=id,
+                                 status=Post.Status.PUBLISHED)
+
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
 # Create your views here.
